1fcae9f2d8b9dac4661fb481d630e213cfd0a328
tanishqsujan <tanishq.kothamasu@gmail.com>
2024-10-12 11:22:13 +0400

    Maggie

---
+++ maggie.java
@@ -0,0 +1,11 @@
+public class maggie {
+    public static void main(String[] args){
+        System.out.println("My Maggie Recipe");
+        Maggie();
+    }
+
+    public static void Maggie(){
+        System.out.println("Boil Water in the pan");
+        System.out.println("Now add your favourite Magie recipe here");
+    }
+}
